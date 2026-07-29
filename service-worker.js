9e912fc5cde0ba14cbe5ec74ef9ca22a1316b4c4
VitãoTub <160746536+vitaotub@users.noreply.github.com>
2026-07-28 23:38:44 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -13,7 +13,7 @@
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
 
 // ==================== CONFIGURAÇÃO DO CACHE ====================
-const CACHE_NAME = 'vitaotub-cache-v0.2';
+const CACHE_NAME = 'vitaotub-cache-v1.0';
 
 // Arquivos para cache inicial (instalação)
 const urlsToCache = [
