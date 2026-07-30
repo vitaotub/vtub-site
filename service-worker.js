c7a5fe61f7dfe7da3242a2486ef68871f39f60ab
VitãoTub <160746536+vitaotub@users.noreply.github.com>
2026-07-30 17:15:32 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,7 @@
  */
 
 // ==================== CONFIGURAÇÃO DO CACHE ====================
-const CACHE_NAME = 'vitaotub-cache-v1.6';
+const CACHE_NAME = 'vitaotub-cache-v1.1';
 
 // Arquivos para cache inicial (instalação)
 const urlsToCache = [
